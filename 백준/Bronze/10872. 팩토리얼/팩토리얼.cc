283ff3fd92ebb7d6861c3f971cb8ad6255350b99
Park Jungho <bagj21563@gmail.com>
2024-03-05 15:25:29 +0900

    [Bronze V] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int num;
+	cin >> num;
+	int result = 1;
+	for (int i = 1; i <= num; i++)
+	{
+		result *= i;
+
+	}
+	cout << result;
+
+}
