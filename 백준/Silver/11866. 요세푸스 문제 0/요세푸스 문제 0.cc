96c2090932a6735e6daddf262fc2929551177de4
Park Jungho <bagj21563@gmail.com>
2024-04-23 15:13:05 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n, k;
+
+	queue<int> q;
+
+
+	int i;
+	cin >> n >> k;
+	for (i = 1; i <= n; i++)
+		q.push(i);
+	cout << "<";
+	while (q.size() != 0)
+	{
+		for (i = 1; i < k; i++)
+		{
+			q.push(q.front());
+			q.pop();
+		}
+		cout << q.front();
+		if (q.size() != 1)
+			cout << ", ";
+		q.pop();
+	}
+	cout << ">";
+
+}
+
+
