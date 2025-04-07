4c71cc67f1b39b2fec6a71babaedc64f49ab5970
Park Jungho <bagj21563@gmail.com>
2025-04-07 21:18:43 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -6,20 +6,20 @@ using namespace std;
 
 const int MAX = 50;
 int T, M, N, K;
-int field[MAX][MAX];
+int board[MAX][MAX];
 bool visited[MAX][MAX];
-int dx[] = {0, 0, 1, -1};
-int dy[] = {1, -1, 0, 0};
+int dx[] = { 0, 0, 1, -1 };
+int dy[] = { 1, -1, 0, 0 };
 
 void dfs(int x, int y) {
     visited[x][y] = true;
-    
+
     for (int i = 0; i < 4; i++) {
         int nx = x + dx[i];
         int ny = y + dy[i];
-        
+
         if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
-            if (field[nx][ny] == 1 && !visited[nx][ny]) {
+            if (board[nx][ny] == 1 && !visited[nx][ny]) {
                 dfs(nx, ny);
             }
         }
@@ -28,35 +28,32 @@ void dfs(int x, int y) {
 
 int main() {
     cin >> T;
-    
+
     while (T--) {
         cin >> M >> N >> K;
-        
-        // 초기화
-        memset(field, 0, sizeof(field));
+
+        memset(board, 0, sizeof(board));
         memset(visited, false, sizeof(visited));
-        
-        // 배추 위치 입력
+
         for (int i = 0; i < K; i++) {
             int x, y;
             cin >> x >> y;
-            field[x][y] = 1;
+            board[x][y] = 1;
         }
-        
+
         int worm_count = 0;
-        
-        // 모든 위치에 대해 DFS 탐색
+
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
-                if (field[i][j] == 1 && !visited[i][j]) {
+                if (board[i][j] == 1 && !visited[i][j]) {
                     dfs(i, j);
                     worm_count++;
                 }
             }
         }
-        
-        cout << worm_count << endl;
+
+        cout << worm_count << '\n';
     }
-    
+
     return 0;
 }
