3c158c6c8d3398b3d6cbc58ba046731b6daed1e6
Park Jungho <bagj21563@gmail.com>
2025-04-07 21:27:31 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,40 +1,47 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
 #include <iostream>
 #include <queue>
-#include <string>
-#include <algorithm>
-#define  MAX 26
-using namespace  std;
+#define MAX 25
+using namespace std;
 
 string arr[MAX];
+
 bool visited[MAX][MAX];
-int n;
-
-int cnt;
-
 
 int dirx[] = { -1,1,0,0 };
 int diry[] = { 0,0,-1,1 };
+int	n;
+int cnt;
+
 void bfs(int x, int y)
 {
 	queue<pair<int, int>> q;
 	q.emplace(x, y);
-
+	visited[x][y] = true;
+	cnt++;
 	while (!q.empty())
 	{
 		pair<int, int> pair = q.front();
 		q.pop();
 		int a = pair.first;
 		int b = pair.second;
-
 		for (int i = 0; i < 4; i++)
 		{
 			int nx = a + dirx[i];
 			int ny = b + diry[i];
 
-			if (nx < 0 || ny < 0 || nx >= n || ny >= n || arr[nx][ny] == '0' || visited[nx][ny])continue;
+			if (nx < 0 || ny < 0 || nx >= n || ny >= n || arr[nx][ny] == '0' || visited[nx][ny])continue;;
 
-			q.emplace(nx, ny);
 			visited[nx][ny] = true;
+			q.emplace(nx, ny);
 			cnt++;
 		}
 	}
@@ -42,13 +49,10 @@ void bfs(int x, int y)
 int main()
 {
 
-	vector<int> vec;
 	cin >> n;
-
+	vector<int> vec;
 	for (int i = 0; i < n; i++)
-	{
 		cin >> arr[i];
-	}
 
 	for (int i = 0; i < n; i++)
 	{
@@ -56,18 +60,17 @@ int main()
 		{
 			if (arr[i][j] == '1' && !visited[i][j])
 			{
-				cnt = 1;
-				visited[i][j] = true;
+				cnt = 0;
 				bfs(i, j);
 				vec.push_back(cnt);
 			}
 		}
 	}
-
 	sort(vec.begin(), vec.end());
 	cout << vec.size() << "\n";
 	for (int i = 0; i < vec.size(); i++)
 	{
 		cout << vec[i] << "\n";
 	}
+
 }
