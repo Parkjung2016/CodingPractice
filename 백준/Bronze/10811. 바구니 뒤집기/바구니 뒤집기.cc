ff84fa56b6952848691b40272548783e0cc91a06
Park Jungho <bagj21563@gmail.com>
2025-04-07 11:09:09 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+#define  MAX 101
+
+int arr[MAX];
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++)
+		arr[i] = i;
+	while (m--)
+	{
+		int i, j;
+		cin >> i >> j;
+		for (int x = i; x <= j; x++)
+		{
+			for (int c = x; c <= j; c++)
+			{
+				int tmp = arr[x];
+				arr[x] = arr[c];
+				arr[c] = tmp;
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+		cout << arr[i] << " ";
+}
