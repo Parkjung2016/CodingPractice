13c947a93a65892330551565673c4cbe6669290d
Park Jungho <bagj21563@gmail.com>
2025-04-09 16:42:51 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <numeric>
+using namespace std;
+#define  MAX 100001
+
+int arr[MAX];
+int n;
+
+void binary_search(int num)
+{
+	int left = 0, right = n - 1;
+
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+
+		if (arr[mid] == num)
+		{
+			cout << 1 << "\n";
+			return;
+		}
+
+		if (arr[mid] > num)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			left = mid + 1;
+		}
+	}
+
+	cout << 0 << "\n";
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	int m;
+	cin >> m;
+	sort(arr, arr + n);
+	for (int i = 0; i < m; i++)
+	{
+		int num;
+		cin >> num;
+		binary_search(num);
+	}
+}
