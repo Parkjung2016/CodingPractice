ae2a57fb97966449aa11fdea76d1cc9a47a00c99
Park Jungho <bagj21563@gmail.com>
2025-04-14 15:24:45 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 296 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -8,32 +8,33 @@
 #include <map>
 #include <numeric>
 using namespace std;
-#define  MAX 500001
 
+#define MAX 500001
 int arr[MAX];
-int n;
-
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
+
+	int n;
 	cin >> n;
 	for (int i = 0; i < n; i++)
 	{
 		cin >> arr[i];
 	}
 
-	sort(arr, arr + n);
 	int m;
 	cin >> m;
-
+	sort(arr, arr + n);
 	for (int i = 0; i < m; i++)
 	{
 		int num;
 		cin >> num;
+
 		int count = upper_bound(arr, arr + n, num) - lower_bound(arr, arr + n, num);
-		cout << count << "\n";
+
+		cout << count << " ";
 	}
 }
