b5b9fa0270b326307756e69f755f4922da26ca89
Park Jungho <bagj21563@gmail.com>
2025-04-14 19:46:01 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <numeric>
+#include <unordered_map>
+using namespace std;
+
+#define MAX 41
+
+int dp[MAX][2];
+
+void f(int n)
+{
+	dp[0][0] = 1;
+	dp[0][1] = 0;
+	dp[1][0] = 0;
+	dp[1][1] = 1;
+
+	for (int i = 2; i <= n; i++)
+	{
+		dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+		dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+	}
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		f(n);
+		cout << dp[n][0] << " " << dp[n][1] << "\n";
+	}
+}
