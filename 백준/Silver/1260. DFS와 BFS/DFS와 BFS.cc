d71e212e8e151f6681a77df888a62a2445a28917
Park Jungho <bagj21563@gmail.com>
2025-04-14 10:43:53 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 5936 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,30 +1,39 @@
+#include <algorithm>
 #include <queue>
 #include <cstring>
+#include <string>
 #include <iostream>
-#include <queue>
-#define  MAX 1001
+#include <vector>
+#include <string>
+#include <map>
+#include <numeric>
 using namespace std;
 
-int n, m, v;
+#define MAX 1001
 int arr[MAX][MAX];
 bool visited[MAX];
+int n, m;
+
 void dfs(int v)
 {
-	visited[v] = true;
 	cout << v << " ";
+	visited[v] = true;
 
 	for (int i = 1; i <= n; i++)
 	{
 		if (arr[v][i] == 1 && !visited[i])
+		{
 			dfs(i);
+		}
 	}
 }
 void bfs(int v)
 {
-	queue<int>	q;
+	queue<int> q;
 	q.push(v);
 	visited[v] = true;
 	cout << v << " ";
+
 	while (!q.empty())
 	{
 		v = q.front();
@@ -33,28 +42,31 @@ void bfs(int v)
 		{
 			if (arr[v][i] == 1 && !visited[i])
 			{
+				q.push(i);
 				visited[i] = true;
 				cout << i << " ";
-				q.push(i);
 			}
 		}
 	}
 }
 int main()
 {
-
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int v;
 	cin >> n >> m >> v;
 
 	for (int i = 0; i < m; i++)
 	{
 		int a, b;
 		cin >> a >> b;
-
 		arr[a][b] = 1;
 		arr[b][a] = 1;
 	}
 	dfs(v);
-	memset(visited, 0, sizeof(visited));
 	cout << "\n";
+	memset(visited, false, sizeof(visited));
 	bfs(v);
+
 }
