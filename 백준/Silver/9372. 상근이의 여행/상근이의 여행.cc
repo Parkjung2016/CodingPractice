5ce269c4cf22c6aeb6c47404e59f5fe7af74b011
Park Jungho <bagj21563@gmail.com>
2025-04-15 16:05:26 +0900

    [Silver IV] Title: 상근이의 여행, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <numeric>
+#include <unordered_map>
+using namespace std;
+
+#define MAX 27
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, m;
+		cin >> n >> m;
+		for (int i = 0; i < m; i++)
+		{
+			int a, b;
+			cin >> a >> b;
+		}
+		cout << n - 1 << "\n";
+
+	}
+	return 0;
+}
