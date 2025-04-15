b9857e750536ea82ce15cc36e3e080ec14179153
Park Jungho <bagj21563@gmail.com>
2025-04-15 20:23:09 +0900

    [Silver I] Title: 경로 찾기, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <numeric>
+#include <unordered_map>
+using namespace std;
+
+#define MAX 101
+
+int arr[MAX][MAX];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+
+	for (int k = 0; k < n; k++)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (arr[i][k] && arr[k][j])
+				{
+					arr[i][j] = 1;
+				}
+			}
+		}
+	}
+
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
