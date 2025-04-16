9798db5cc42c4577d9c95aeb01b492a17d100b38
Park Jungho <bagj21563@gmail.com>
2025-04-17 08:16:58 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <climits>
+using namespace   std;
+#define  MAX 9
+bool visited[MAX];
+int arr[MAX] = { 0, };
+int n, m;
+
+void dfs(int cnt, int start)
+{
+	if (cnt == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << arr[i] << ' ';
+
+		}
+		cout << "\n";
+		return;
+
+	}
+
+	for (int i = start; i <= n; i++)
+	{
+
+		visited[i] = true;
+		arr[cnt] = i;
+		dfs(cnt + 1, i);
+		visited[i] = false;
+
+	}
+}
+int main()
+{
+	cin >> n >> m;
+	dfs(0, 1);
+	return 0;
+}
