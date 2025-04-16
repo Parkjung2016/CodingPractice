54a14793dd74d2c1aa8fc0a8af5cf5fc288453d5
Park Jungho <bagj21563@gmail.com>
2025-04-16 12:30:43 +0900

    [Silver III] Title: 주유소, Time: 0 ms, Memory: 2052 KB, Score: 41 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <map>
+#include <queue>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <climits>
+#define MAX 2001
+using namespace std;
+
+long long roadLength[MAX];
+long long price[MAX];
+int main()
+{
+	long long n;
+	cin >> n;
+	for (int i = 0; i < n - 1; i++)
+	{
+		cin >> roadLength[i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> price[i];
+	}
+	long long minValue = INT_MAX;
+	int result = 0;
+	for (int i = 0; i < n; i++)
+	{
+		minValue = min(minValue, price[i]);
+
+		result += minValue * roadLength[i];
+
+	}
+
+	cout << result;
+	return 0;
+
+}
